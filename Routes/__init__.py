f714a293ede44511a55f50e733c4f30d28192743
Kalyan <kalyanthiriveedhi2000@gmail.com>
2022-12-31 14:53:32 +0530

    Code for localhost only

---
+++ Routes/__init__.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from mongoengine import connect
+from flask import session
+
+
+DB_NAME="Blog_DB"  #database name
+
+
+def create_app():
+    app=Flask(__name__)
+    app.secret_key="sessionkey"     # for sessions
+    app.config['SECRET_KEY'] ="BLOGWebsite"
+ 
+    try:
+        db=connect(DB_NAME,host="mongodb://localhost:27017")    #  initializing the MongoDB with the flask application(app).
+    except ConnectionError as e:
+        print("Error while connecting to the Database : "+e)
+    
+    from .views import views
+    from .auth import auth
+    
+    app.register_blueprint(views,url_prefix="/") 
+    app.register_blueprint(auth,url_prefix="/")
+
+    return app
+
